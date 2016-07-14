3f7c989866132f3c67e4bdff3a1ebaaa2ea239b1
Chris Bergstrom <chris@snapcare.io>
2016-07-13 22:25:15 -0700

    Prepping for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,29 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
-var serve = require('gulp-serve');
-var config = require('config');
 var jsonServer = require('json-server');
-var server = require('gulp-express');
 
 // Webpack
-gulp.task('webpack', function () {
+gulp.task('build', function () {
   return gulp.src('./src/app.js')
     .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./public'));
 });
 
 // JSON API Server - run a REST server via a simple json file
-gulp.task('serve:api', function (cb) {
+gulp.task('serve', function (cb) {
   var apiServer = jsonServer.create();
   var router = jsonServer.router('db.json');
 
   apiServer.use(jsonServer.defaults());
   apiServer.use(router);
-  apiServer.listen(config.get('api.port'));
+  apiServer.listen(process.env.PORT || 8000);
 
   cb();
 });
 
-// // Simple Server
-// gulp.task('serve:web', serve({
-//   root: './dist',
-//   port: config.get('server.port')
-// }));
-
-gulp.task('serve:web', function() {
-  server.run(['server.js'])
-});
-
 // Watch for changes and reload stuff
 gulp.task('watch', function () {
-  gulp.watch('./src/**/*', ['webpack']);
+  gulp.watch('./src/**/*', ['build']);
 });
 
-gulp.task('default', ['webpack', 'serve:web', 'serve:api', 'watch']);
+gulp.task('default', ['build', 'serve', 'watch']);
